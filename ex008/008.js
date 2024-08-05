f1a4123abd4e357a0f78f25122e77e9ed57a107a
Emerson Willian <emersonwdias2013@gmail.com>
2024-08-05 15:47:51 -0300

    Initial commit

---
+++ ex008/008.js
@@ -0,0 +1,7 @@
+var vel = 51
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+} else{
+    console.log(`Dirija sempre usando cinto de segurança!`)
+}
